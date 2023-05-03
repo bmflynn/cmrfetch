753d11624f2da0dedab9f967a646f9a8e28397fb
Bruce Flynn <brucef@ssec.wisc.edu>
2023-05-03 10:58:19 -0500

    morph build.sh script into a local/dev build script

--- build.sh
+++ build.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
+#
+# Build script for local or dev builds. Production or release builds should be 
+# handled via the SLA Go Releaser GitHub Action.
+#
 set -e
-export VER=`git describe --dirty`
-export GOOS=linux
-export GOARCH=amd64
-export CGO_ENABLED=0
+export builddir=./build
+
+commit_date=$(git log --date=iso8601-strict -1 --pretty=%ct)
+commit=$(git rev-parse HEAD)
+version=$(git describe --tags --always --dirty | cut -c2-)
+tree_state=$(if git diff --quiet; then echo "clean"; else echo "dirty"; fi)
 
 function build() {
   export GOOS=$1
   export GOARCH=$2
-  local sfx=${GOOS}_${GOARCH}
+  export CGO_ENABLED=0
+
   pkg=github.com/bmflynn/cmrfetch/internal
   buildtime=$(date -u +%Y-%m-%dT%H:%M:%SZ)
   sha=$(git rev-parse HEAD)
 
-  binname=cmrfetch
+  binname=cmrfetch-${GOOS}-${GOARCH}
   if [[ $GOOS == "windows" ]]; then
     binname=${binname}.exe
   fi
-  outdir=./build/cmrfetch_${sfx}
-  mkdir -p ${outdir}
-  go build \
-    -o ${outdir}/${binname} \
-    -ldflags "-s -w -X ${pkg}.Version=${VER} -X ${pkg}.BuildTime=${buildtime} -X ${pkg}.GitSHA=${sha} --extldflags '-static'"
-  cp README.md ${outdir}
-  (
-    cd build
-    tar -cvzf cmrfetch_${sfx}.tar.gz $(basename ${outdir})
-  )
-  rm -rf ${outdir}
+    
+  ldflags="-s -w --extldflags '-static'"
+  ldflags="${ldflags} -X github.com/bmflynn/cmrfetch/internal.Version=${version}"
+  ldflags="${ldflags} -X github.com/bmflynn/cmrfetch/internal.Commit=${commit}"
+  ldflags="${ldflags} -X github.com/bmflynn/cmrfetch/internal.CommitDate=${commit_date}"
+  ldflags="${ldflags} -X github.com/bmflynn/cmrfetch/internal.TreeState=${tree_state}"
+  go build -o ${builddir}/${binname} -ldflags "${ldflags}"
+  #  -ldflags "-s -w -X ${pkg}.Version=${VER} -X ${pkg}.BuildTime=${buildtime} -X ${pkg}.GitSHA=${sha} --extldflags '-static'"
 }
 
-mkdir -pv build
+mkdir -pv $builddir
+rm -fv $builddir/*
+
 build linux amd64
 build windows amd64
 build darwin amd64
